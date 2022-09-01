b8b175d1053043c791811b28781fc399b7bb428a
reycardo <reycardo_95@hotmail.com>
2022-09-01 19:55:12 +0100

    clarify

--- src/bot.py
+++ src/bot.py
@@ -2,42 +2,43 @@ import os
 import discord
 from discord import Embed
 from dotenv import load_dotenv
-from scrap import Cine_Movies
+from scrap import Cine_Scraper
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
 
-
+# message content intent is being enforced
 intents = discord.Intents.default()
 intents.message_content = True
+
+# Init bot with intents
 bot = discord.Bot(intents=intents)
 
+# Init scraper
+scraper = Cine_Scraper()
 
-cine = Cine_Movies()
-
+# So we know the bot is ready
 @bot.event
 async def on_ready():
     print(f"We have logged in as {bot.user}")
 
 @bot.event
 async def on_message(message):
+    
+    # so bot isn't listening to himself
     if message.author == bot.user:
-        return
+        return            
     
-    message_content = message.content.lower()  
-    
+    # responds with Hello! to a $hello
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
     
-    if f'$cinema' in message_content:
-        cinema = ' '.join(message_content.split()[1:])
-        await message.channel.send(f'set cinema to: {cinema}')
-
-    if f'$search' in message_content:
+    # searches for the days/hours the movie in message will screen in cinema
+    if message.content.startswith('$search'):    
         cinema = 'vasco da gama'
-        key_words, search_words = cine.key_words_search_words(message_content)
-        dates_hours,image = cine.search_film(key_words,cine=cinema)
+        key_words, search_words = scraper.key_words_search_words(message.content)
+        dates_hours,image = scraper.search_film(key_words,cine=cinema)
         embed = Embed()
         fields = [(f"**{key}**", f"{value}", False) for key, value in dates_hours.items()]
         for name, value, inline in fields:
--- src/scrap.py
+++ src/scrap.py
@@ -1,11 +1,10 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-class Cine_Movies:
+class Cine_Scraper:
     def __init__(self):        
-        self.base_url = 'https://cinemas.nos.pt'
-        self.cinemas_prefix = '/Cinemas/Pages/'        
-        self.filmes_prefix = '/Filmes/Pages/'
+        self.base_url = 'https://cinemas.nos.pt'        
+        self.page_prefix = '/Filmes/Pages/'
         self.page_suffix = '.aspx'
     
     def key_words_search_words(self, user_message):                
@@ -17,20 +16,31 @@ class Cine_Movies:
         return keywords, search_words
     
 
-    def search_film(self, keywords, cine):
-        """Searches for the film
-        """
-        response = requests.get(self.base_url+self.filmes_prefix+keywords+self.page_suffix)
+    def search_movie(self, keywords, cine):
+        """Searches for the movie
+        """                
+        response = requests.get(self.base_url+self.page_prefix+keywords+self.page_suffix)
         content = response.content
-        soup = bs(content, 'html.parser')        
-        articles = soup.findAll('article', class_ ='line')
+        soup = bs(content, 'html.parser')                
+        
+        # Finds the image of the movie
+        image = soup.find('article', class_='details--left').find('img')['src']
+
+        # Finds all dates 
         available_dates = [[date for date in date_options.stripped_strings] for date_options in soup.find('select').findAll('option')]                
+        
+        # Finds all articles
+        articles = soup.findAll('article', class_ ='line')        
         movie_hours = []
+        
+        # Appends in movie_hours every hour of every date  for the specific cinema
         for article in articles:
             if cine in ''.join([[y for y in x.stripped_strings] for x in article.children][1]).lower():
                 lst = [[y for y in x.stripped_strings] for x in article.find('div', class_ = 'hours').findAll('a')]
                 hours = [item[0] for item in lst]
                 movie_hours.append(hours)
+        
+        # Turns available_dates and movie_hours into key,val dict
         dates_hours = {key: value for keys, value in zip(available_dates, movie_hours) for key in keys}
-        image = soup.find('article', class_='details--left').find('img')['src']
+        
         return dates_hours,self.base_url+image
